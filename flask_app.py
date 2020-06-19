06d8bc465b5d509537e708d04f1486740eebc871
Yann <yann.scheiffer@polymtl.ca>
2020-06-19 15:08:23 -0400

    add flask_app.py to connect with pythoneverywhere

---
+++ flask_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    return render_template("index.html")
+
+@app.route('/contact', methods=['POST','GET'])
+def contact():
+    return render_template("contact.html")
